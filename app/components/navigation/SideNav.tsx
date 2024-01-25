f4d0eab8127f3bcbafff9e84ea2fab809ac97bc2
Saumy Pandey <saumyp@infinite.com>
2024-01-25 21:54:10 +0530

    Fixed launch date and alignment of top and side bar

--- app/components/navigation/SideNav.tsx
+++ app/components/navigation/SideNav.tsx
@@ -1,38 +1,50 @@
 "use client";
-import Image from "next/image";
-import styles from "./Navigation.module.css";
-import Link from "next/link";
+
 import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./Navigation.module.css";
 
 
 const SideNav = () => {
-  const [showSideNav, setShowSideNav] = useState<boolean>(false);
+
+  const [showSideNav, setShowSideNav] = useState(false);
+
   const handleHover = () => {
     setShowSideNav(true);
   };
+
   const handleLeave = () => {
     setShowSideNav(false);
   };
+
   return (
+
     <div className={styles.sideNavContainer}>
       <div>
         <Image src="/side_logo.png" width={70} height={40} alt="side logo" />
       </div>
       <div className={styles.iconContainer} onMouseOver={handleHover}>
+
         <Link href={"/"}>
           <Image src="/home.svg" width={24} height={24} alt="home" />
         </Link>
+
         <Image src="/msg_box.svg" width={24} height={24} alt="msg box" />
         <Image src="/store.svg" width={24} height={24} alt="store" />
         <Image src="/payment.svg" width={24} height={24} alt="card" />
         <Image src="/app_store.svg" width={24} height={24} alt="app store" />
         <Image src="/trophy.svg" width={24} height={24} alt="trophy" />
+
       </div>
+
       <div className={styles.hrBorder}></div>
       <div className={styles.iconContainer} onMouseOver={handleHover}>
         <Image src="/setting.svg" width={24} height={24} alt="setting" />
         <Image src="/logout.svg" width={24} height={24} alt="logout" />
       </div>
+
       {showSideNav && (
         <div className={styles.SideNav} onMouseLeave={handleLeave}>
           <div className={styles.sideImageContainer}>
